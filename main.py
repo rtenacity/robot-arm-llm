7b61fe9eadaf3d4fc5fc8155752cf5c6491692d3
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2023-12-06 10:26:06 -0500

    thing

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, LargeMotor, MediumMotor
-from ev3dev2.sensor.lego import GyroSensor
+from ev3dev2.sensor.lego import GyroSensor, TouchSensor
+from ev3dev2.button import Button
+from time import sleep
 
 revolver = LargeMotor(OUTPUT_A)
 shoulder = LargeMotor(OUTPUT_B)
 elbow = LargeMotor(OUTPUT_C)
 claw = MediumMotor(OUTPUT_D)
 gyro = GyroSensor()
+touch = TouchSensor()
+gyro.MODE_GYRO_G_A()
+btn = Button()
+motor_list = [revolver, shoulder, elbow, claw]
 
-def calibrate(motor, angle, vel):
-    while motor.position() < angle or motor.position() > 0:
-        if motor.position() < angle:
-            motor.on(vel)
-        else: 
-            revolver.on(-vel)
+
+def calibrate():
+    for motor in motor_list:
+        calibrated = False
+        while calibrated == False:
+            if btn.left():
+                motor.on_for_degrees(50, 10)
+            elif btn.right():
+                motor.on_for_degrees(50, -10)
+            elif btn.up():
+                motor.on_for_degrees(20, 2)
+            elif btn.down():
+                motor.on_for_degrees(20, -2)
+            elif btn.enter():
+                calibrated = True
             
-
 def initialize(cal_speed):
-    calibrate(revolver, 0, cal_speed)   
-    print("revolver calibrated")
+    calibrate()
+    touch.wait_for_bump()
+    gyro.calibrate()
     
-    calibrate(shoulder, 90, cal_speed)
-    print("shoulder calibrated")
-    
-    calibrate(elbow, 90, cal_speed)
-    print("elbow calibrated")
-    
-    calibrate(claw, 0, cal_speed*0.5)
-    print("claw calibrated")
-    
+def move_to_point(x, y, z, speed=50):
+    pass
 
 def main():
     pass
