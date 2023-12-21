f08843e60c7662c97b6dd1397f9c41173a49f1f0
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2023-12-21 12:42:33 -0500

    tings

--- bot.py
+++ bot.py
@@ -81,13 +81,6 @@ class Bot:
         self.calibrate()
         self.calculate_position()
         
-    def calibrate_sus(self):
-        self.shoulder.reset()
-        sleep(2)
-        self.shoulder.on_for_degrees(100, 100)
-        self.shoulder.reset()
-        self.shoulder.on_for_degrees(50, 0)
-        
         
     def move_to_point(self, target_pos):
         
@@ -97,18 +90,14 @@ class Bot:
         tempX += self.back_offset*math.sin(math.radians(alpha))
         tempY += self.back_offset*math.cos(math.radians(alpha))
         target_pos = Position(tempX, tempY, tempZ)
-        print(target_pos)
         
         target_length = get_length(Position(0, 0, 0), target_pos)
         a, b, c = get_angles_triangle(self.shoulder_length, self.elbow_length, target_length)
         b_theta = (45 + self.shoulder.position) - b # temp
 
-        c_theta = (135 + self.elbow.position) - c # temp
-        
+        c_theta = (135 + self.elbow.position) - c # temp    
         self.shoulder.on_for_degrees(5, -b_theta)
-        
         self.elbow.on_for_degrees(5, -c_theta)
-        
         self.calculate_position()
         xy_theta, yz_theta = angles_to_move(self.pos, target_pos)
         print(b_theta, yz_theta, xy_theta)
