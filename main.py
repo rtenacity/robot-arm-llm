8487e43f65880e8e47c01f95d16e175ad26dd00e
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2023-12-29 14:53:02 -0500

    https://www.youtube.com/watch?v=g_wlZ9IhbTs

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 from bot import Bot
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D
 from position import Position
-
 arm = Bot(OUTPUT_A, OUTPUT_C, OUTPUT_B, OUTPUT_D)
 
-arm.initialize()
-print(arm.get_pos())
-arm.move_to_point(Position(0, 6, 0))
-print(arm.get_pos())
+def main():
+    arm.initialize()
+    print(arm.get_pos())
+    arm.move_to_point(Position(0, 6, 0))
+    print(arm.get_pos())
+    
+if __name__ == '__main__':
+    main()
 
 # ok so it kind of works now i just need to tune values!!!!!!
 # also in order to get thing to work need to slower speed of motors especially shoulder
